0e7d4abbdeedc252774d54b7488e0de762e08348
DJTB <duncan.bay@gmail.com>
2016-04-19 14:46:49 +1000

    useref depends on less task finishing first - they were running in parallel previously

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('useref', function(){
+gulp.task('useref', ['less'], function(){
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify({
@@ -47,7 +47,7 @@ gulp.task('watch', ['browserSync', 'less'], function(){
 });
 
 gulp.task('build', ['clean:dist'], function (){
-  gulp.start('less', 'useref');
+  gulp.start('useref');
   console.log('Building files');
 });
 
