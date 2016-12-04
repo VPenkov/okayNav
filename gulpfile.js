717c05fd1be4739a1a0a900ecaab705618f183f3
Vergil Penkov <vergil@moongrab.com>
2016-12-04 19:04:45 +0200

    Initial boilerplate code

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,92 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var del = require('del');
-var cssnano = require('gulp-cssnano');
-var autoprefixer = require('gulp-autoprefixer');
+'use strict';
 
-gulp.task('less', function() {
-  return gulp.src(['app/less/okayNav-base.less', 'app/less/okayNav-theme.less']) // Get source files with gulp.src
-    .pipe(less())
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+var Autoprefixer = require('gulp-autoprefixer');
+var Browserify = require('browserify');
+var BrowserSync = require('browser-sync');
+var buffer = require('buffer');
+var Eslint = require('eslint');
+var Gulp = require('gulp');
+var Plumber = require('plumber');
+var Sass = require('gulp-sass');
+var Sourcemaps = require('gulp-sourcemaps');
+var Stylelint = require('stylelint');
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
+var reload = BrowserSync.reload;
+var autoPrefixOptions = {
+    browsers: [
+        '> 1%',
+        'last 2 versions',
+        'Firefox ESR'
+    ]
+};
+
+var cssNanoSettings = {
+    safe: true,
+    autoprefixer: false // done before minification
+};
+
+var folders = {
+    dev: {
+        base: 'app',
+        css: 'app/scss',
+        js: 'app/js'
     },
-  })
+    dist: {
+        base: 'dist',
+        css: 'dist/css',
+        js: 'dist/js'
+    }
+};
+
+Gulp.task('build:css', function() {
+    return Gulp
+        .src(`${folders.dev.css}/**/*.scss`)
+        .pipe(Plumber())
+        .pipe(Sourcemaps.init())
+        .pipe(Sass().on('error', Sass.logError))
+        .pipe(Autoprefixer(autoPrefixOptions))
+        .pipe(Sourcemaps.write())
+        .pipe(Gulp.dest(`${folders.dist.css}//**/*.scss`))
+        .pipe(reload({stream: true}));
 });
 
-gulp.task('useref', ['less'], function(){
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify({
-        preserveComments: 'license'
-    })))
-    .pipe(gulpIf('*.css', cssnano({autoprefixer: false})))
-    .pipe(gulp.dest('dist'))
+Gulp.task('build:js', function() {
+    var APP_SOURCE = 'okayNav.js';
+
+    var okayNav = Browserify({
+        entries: `${folders.dev.js}/${APP_SOURCE}`
+    });
+
+    return function() {
+        okayNav.bundle()
+            .pipe(source(APP_SOURCE))
+            .pipe(Plumber())
+            .pipe(buffer())
+            .pipe(Sourcemaps.init({loadMaps: true}))
+            .pipe(Sourcemaps.write('.'))
+            .pipe(Gulp.dest(`${folders.dist.js}`))
+            .pipe(reload({stream: true}));
+    };
 });
 
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
+Gulp.task('lint:css', function() {
+    return Gulp
+        .src(`${folders.dev.css}/**/*.scss`)
+        .pipe(Stylelint({
+            syntax: "scss",
+            reporters: [{
+                formatter: 'string',
+                console: true
+            }]
+        }));
 });
 
-gulp.task('watch', ['browserSync', 'less'], function(){
-  gulp.watch('app/less/**/*.less', ['less', browserSync.reload]);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-  gulp.watch('app/*.html', browserSync.reload);
-});
-
-gulp.task('build', ['clean:dist'], function (){
-  gulp.start('useref');
-  console.log('Building files');
-});
-
-// Default task
-gulp.task('default', function() {
-  gulp.start('build');
+Gulp.task('lint:js', function() {
+    return function() {
+        Gulp.src(`${folders.dev.js}/**/*.js`)
+            .pipe(reload({stream: true, once: true}))
+            .pipe(Eslint())
+            .pipe(Eslint.format())
+            .pipe(Gulp.dest(`${folders.dev.js}`));
+    };
 });
